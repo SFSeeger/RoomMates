04e1dfebf1d93e0c9ea3c61d4e7df24f0f17227e
Mara-161 <mara_sack@icloud.com>
2026-02-23 17:08:51 +0100

    fix: event sinnvoller aufgeteilt

--- packages/frontend/src/components/ui/events/eventlistentry.rs
+++ packages/frontend/src/components/ui/events/eventlistentry.rs
@@ -14,101 +14,7 @@ use dioxus_free_icons::icons::ld_icons::{
 use time::macros::format_description;
 
 #[component]
-pub fn EventList() -> Element {
-    let mut events = use_loader(move || async move { list_events().await })?;
-    let mut event_has_groups = use_action(event_has_groups);
-    let mut delete_event = use_action(delete_event);
-    let mut remove_event_from_group = use_action(remove_event_from_group);
-    let mut toaster = use_toaster();
-
-    let ondelete = move |event_id: i32| async move {
-        {
-            event_has_groups.call(event_id).await;
-            if let Some(Ok(groups)) = event_has_groups.value() {
-                if groups.read().is_empty() {
-                    delete_event.call(event_id).await;
-                    match delete_event.value() {
-                        Some(Ok(_)) => {
-                            toaster.success("Deleted event successfully!", ToastOptions::new());
-                            events.write().retain(|event| event.id != event_id);
-                        }
-                        Some(Err(_)) => {
-                            toaster.error("Failed to delete event!", ToastOptions::new());
-                        }
-                        None => {
-                            warn!("Request did not finish!");
-                        }
-                    }
-                } else {
-                    for group in groups.read().iter() {
-                        remove_event_from_group.call(group.id, event_id).await;
-                        match remove_event_from_group.value() {
-                            Some(Ok(_)) => {
-                                toaster.success("Deleted event successfully!", ToastOptions::new());
-                                events.write().retain(|event| event.id != event_id);
-                            }
-                            Some(Err(_)) => {
-                                toaster.error("Failed to delete event!", ToastOptions::new());
-                            }
-                            None => {
-                                warn!("Request did not finish!");
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    };
-
-    rsx! {
-        div { class: "w-full",
-            List { header: "Your Events",
-                for event in events.iter() {
-                    EventListEntry { event: event.clone(), ondelete }
-                }
-            }
-        }
-    }
-}
-
-#[component]
-pub fn EventListGroups(group_id: i32) -> Element {
-    let mut group = use_loader(move || async move { retrieve_group(group_id).await })?;
-    let mut remove_event_from_group = use_action(remove_event_from_group);
-    let mut toaster = use_toaster();
-
-    let ondelete = move |event_id: i32| async move {
-        {
-            remove_event_from_group.call(group_id, event_id).await;
-            match remove_event_from_group.value() {
-                Some(Ok(_)) => {
-                    toaster.success("Deleted event successfully!", ToastOptions::new());
-                    group.write().events.retain(|event| event.id != event_id);
-                }
-                Some(Err(_)) => {
-                    toaster.error("Failed to delete event!", ToastOptions::new());
-                }
-                None => {
-                    warn!("Request did not finish!");
-                }
-            }
-        }
-    };
-
-    rsx! {
-        div { class: "w-full",
-            List { header: "Your Events",
-                for event in group.read().events.iter() {
-                    EventListEntry { event: event.clone(), ondelete }
-                }
-            }
-        }
-    }
-}
-
-#[component]
 pub fn EventListEntry(event: entity::event::Model, ondelete: EventHandler<i32>) -> Element {
-    let event_has_groups = use_loader(move || async move { event_has_groups(event.id).await })?;
     let title = event.title.clone();
     let start = event
         .start_time
@@ -184,18 +90,6 @@ pub fn EventListEntry(event: entity::event::Model, ondelete: EventHandler<i32>) 
                                 Icon { icon: LdPencil }
                                 "Edit"
                             }
-                            Dialog {
-                                DialogTrigger {
-                                    variant: ButtonVariant::Primary,
-                                    ghost: false,
-                                    class: "btn",
-                                    Icon { icon: LdUsers }
-                                    "Add to group"
-                                }
-                                DialogContent { title: "Choose a group you want to add this event to",
-                                    AddEventToGroup {}
-                                }
-                            }
                             Button {
                                 onclick: move |_| { ondelete.call(event.id) },
                                 variant: ButtonVariant::Error,
@@ -206,15 +100,5 @@ pub fn EventListEntry(event: entity::event::Model, ondelete: EventHandler<i32>) 
                 }
             }
         }
-    }
-}
-
-#[component]
-pub fn AddEventToGroup() -> Element {
-    let _toaster = use_toaster();
-    let _dialog = use_dialog();
-
-    rsx! {
-        div {}
     }
 }
